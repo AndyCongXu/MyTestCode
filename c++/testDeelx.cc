03fa1ba37e1a99d4402715a77bee2bd3e505d438
AndyCongXu <846352054@qq.com>
2016-08-03 18:38:42 +0800

    add regularly expression

--- c++/testDeelx.cc
+++ c++/testDeelx.cc
@@ -11,15 +11,21 @@ int main(int argc, char **argv)
 {
     CRegexpT<char> pRegExp("((\\+?[0-9]+,)*(\\+?[0-9]+))?");
     //std::auto_ptr<char> ptrRegExp(pRegExp);
-    char *testStr = "+12,2,+36";
-
+    //char *testStr = "+12,2,+36";
+    //char *testStr = "+411,41,041";
+    char *testStr = new char(1024);
+    std::auto_ptr<char> ptrTestStr(testStr);
+    
+    std::cin >> testStr;
     MatchResult anIntResult = pRegExp.MatchExact(testStr);
     std::cout << "XXXX anIntResult = " << anIntResult.IsMatched() << std::endl;
 
     char *testStr1 = "";
+    std::auto_ptr<char> ptrTestStr1(testStr1);
 
     anIntResult = pRegExp.MatchExact(testStr1);
     std::cout << "XXXX anIntResult = " << anIntResult.IsMatched() << std::endl;
+
 
 
     return 0;
